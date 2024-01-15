60201090cabe5ba95bb2eee0ea41e2d90ed4eece
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-01-15 12:13:22 +0300

    Add files via upload

---
+++ satranc_saati_1/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace satranc_saati_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.Location = new System.Drawing.Point(51, 57);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(319, 234);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 355);
+            this.Controls.Add(this.btnBasla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ satranc_saati_1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace satranc_saati_1
+{
+    public partial class Form1 : Form
+    {
+        int sureBeyaz = 10, sureSiyah = 10;
+        bool beyazMi = true;
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            if(sureBeyaz>0|| sureSiyah > 0)
+            {
+                timer1.Start();
+            }
+            if (beyazMi)
+            {
+                beyazMi = false;
+            }
+            else
+            {
+                beyazMi = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (sureBeyaz <= 0 && timer1.Enabled)
+            {
+                MessageBox.Show("Siyah Kazandı");
+                timer1.Stop();
+            }
+            if(sureSiyah <= 0)
+            {
+                MessageBox.Show("Beyaz Kazandı");
+                timer1.Stop();
+            }
+            if (beyazMi == false)
+            {
+                sureBeyaz--;
+                btnBasla.Text=$"Beyaz için Kalan sure:{sureBeyaz}";
+                this.BackColor = Color.White;
+            }
+            else
+            {
+                sureSiyah--;
+                btnBasla.Text=$"Siyah için Kalan sure:{sureSiyah}";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+    }
+}
